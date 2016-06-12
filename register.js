4318418fcf7811d692c5c50ad8ce48b316b1c8aa
ywang85 <ywang85@slu.edu>
2016-06-12 15:33:53 -0500

    student version

--- register.js
+++ register.js
@@ -5,23 +5,26 @@ var newRealName;
 var newUserName;
 
 function submit () {
-	newUserName=document.getElementById("newUserName").value;
-	newPassword=document.getElementById("newPassword").value;
-	newBannerId=document.getElementById("newBanner").value;
-	newUserType=document.getElementById("newUserType").value;
-	newRealName=document.getElementById("newRealName").value;
-	if (isValid(newPassword)==true && isValid(newBannerId)==true && isValid(newUserType)==true && isValid(newRealName)==true)
-		alert("complete");
+    newUserName=document.getElementById("newUserName").value;
+    newPassword=document.getElementById("newPassword").value;
+    newBannerId=document.getElementById("newBanner").value;
+    newUserType=document.getElementById("newUserType").value;
+    newFirstName=document.getElementById("newFirstName").value;
+    newLastName=document.getElementById("newLastName").value;
+    if (isValid(newPassword)==true && isValid(newBannerId)==true && isValid(newUserType)==true && isValid(newFirstName)==true && isValid(newLastName)==true){
+	alert("complete");
+	window.location="loginWindow.html";
+    }
 }
 
 function isValid (context){
-	if (!/^[a-zA-Z0-9]+$/.test(context)){
-		alert("not valid");
-		return false;
-	}
-	if (context.length>16 || context.length==0){
-		alert("not valid");
-		return false;
-	}
-	return true;
+    if (!/^[a-zA-Z0-9]+$/.test(context)){
+	alert("not valid");
+	return false;
+    }
+    if (context.length>16 || context.length==0){
+	alert("not valid");
+	return false;
+    }
+    return true;
 }
