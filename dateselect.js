698664420bd093925e2d0634ae7bdd640b0e5bc4
10633 <lchen22@linuxlab08.mcs.slu.edu>
2016-06-13 17:59:01 -0500

    add dateselector

--- dateselect.js
+++ dateselect.js
@@ -1,7 +1,3 @@
-<html>
-  <head>
-    <title>Add Homework</title>
-    <script type="text/javascript"> 
 function DateSelector(selYear, selMonth, selDay) { 
 this.selYear = selYear; 
 this.selMonth = selMonth; 
@@ -111,14 +107,3 @@ this.InitDaySelect();
 // 设置天数初始值 
 this.selDay.selectedIndex = day - 1; 
 } 
-</script>
-  </head>
-  <h4>Term: </h4>
-  <h4>Course Number: </h4>
-  <h4>Course Name: </h4>
-  <h4>Section: </h4>
-  <h4>Due date: </h4>
-  <select id="selYear"><option value="2016">2016</option></select>
-  <select id="selMonth"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option></select>
-  <select id="selDay"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option><option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option><option value="30">30</option><option value="31">31</option></select>
-</html>
