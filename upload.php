964af8edb3843ba69fa71b7057d2f248458b1575
10633 <lchen22@turing.slu.edu>
2016-06-29 11:57:01 -0500

    add upload function

--- upload.php
+++ upload.php
@@ -1,20 +1,12 @@
 <?php
-if ($_FILES["file"]["error"] > 0){
-	echo "Error: " . $_FILES["file"]["error"] . "<br />";
-}
-else{
-  echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-  echo "Type: " . $_FILES["file"]["type"] . "<br />";
-  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-  echo "Stored in: " . $_FILES["file"]["tmp_name"];
+$strServer = 'turing.slu.edu';
+$strServerPort = '22';
+$strServerUsername = 'username';
+$strServerPassword = 'password';
 
-  if (file_exists("upload/" . $_FILES["file"]["name"])){
-    echo $_FILES["file"]["name"] . " already exists. ";
-  }
-  else{
-    move_uploaded_file($_FILES["file"]["tmp_name"],
-    "upload/" . $_FILES["file"]["name"]);
-    echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-  }
-}
-?>
+$resConnection = ssh2_connect($strServer, $strServerPort);
+
+if(ssh2_auth_password($resConnection, $strServerUsername, $strServerPassword)){
+$resSFTP = ssh2_sftp($resConnection);
+echo $resSFTP;
+$filename = '
