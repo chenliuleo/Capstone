c3766cd1fb5e0ea05cdb03b4dd8c55953a2e6749
10633 <lchen22@turing.slu.edu>
2016-06-29 12:10:41 -0500

    add cgi upload conf.

---
+++ WWW/cgi-bin/cgitest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+print "Content-type:text/html"
+print  # 空行，告诉服务器结束头部
+print '<html>'
+print '<head>'
+print '<meta charset="utf-8">'
+print '<title>CGI TEST</title>'
+print '</head>'
+print '<body>'
+print '<h2>CGI TEST</h2>'
+print '</body>'
+print '</html>'
---
+++ WWW/cgi-bin/save_file.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import cgi, os
+import cgitb; cgitb.enable()
+ 
+form = cgi.FieldStorage()
+ 
+# 获取文件名
+fileitem = form['filename']
+ 
+# 检测文件是否上传
+if fileitem.filename:
+   # 设置文件路径 
+   fn = os.path.basename(fileitem.filename)
+   open('/tmp/' + fn, 'wb').write(fileitem.file.read())
+ 
+   message = 'The file "' + fn + '" was uploaded successfully'
+ 
+else:
+   message = 'No file was uploaded'
+ 
+print """\
+Content-Type: text/html\n
+<html>
+<body>
+   <p>%s</p>
+</body>
+</html>
+""" % (message,)
