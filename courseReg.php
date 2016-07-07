0ef86edf9e6588fc61899b315894c48104698a51
ywang85 <ywang85@slu.edu>
2016-07-06 19:06:11 -0500

    course reg

---
+++ courseReg.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title> Course Registration </title>
+  </head>
+  <body>
+    <center> <h1> Course Registration </h1> <center>
+    <p>
+      Semester:
+      <select name="snedmessage">
+        <option selected>2016</option>
+	<option>2017</option>
+	<option>2018</option>
+      </select>
+      <input type="submit" value="Submit">
+      <input type="reset" value="Reset">
+    </p>
+  </body>
+ </html>
