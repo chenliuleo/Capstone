9a8c77de95b8719d998d3752eb8254dc595e1eb6
10633 <lchen22@turing.slu.edu>
2016-07-25 12:30:15 -0500

    redesigned database.

---
+++ database.sql
@@ -0,0 +1,54 @@
+DROP SCHEMA IF EXISTS csp_homework;
+CREATE SCHEMA  csp_homework;
+USE csp_homework;
+
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email_creds VARCHAR(32) NOT NULL UNIQUE, -- credantials only (like gpeter12, not gpeter12@slu.edu)
+	first_name NVARCHAR(32),
+	last_name NVARCHAR(32),
+	status ENUM('student', 'faculty') -- student = 1, faculty = 2
+);
+
+CREATE TABLE courses (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name NVARCHAR(8) NOT NULL,
+	section NVARCHAR(2) NOT NULL,
+	description TEXT,
+	start_date DATE,
+	end_date DATE,
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE homework (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	title NVARCHAR(64) NOT NULL,
+	description TEXT,
+	course_id INT,
+	deadline DATETIME NOT NULL,
+	active BOOLEAN DEFAULT TRUE,
+	FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
+) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE files (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	filename NVARCHAR(256) NOT NULL,
+	submit_time DATETIME DEFAULT NOW(),
+	student_id INT,
+	hw_id INT,
+	FOREIGN KEY (student_id) REFERENCES users(id) ON DELETE CASCADE,
+	FOREIGN KEY (hw_id) REFERENCES homework(id) ON DELETE CASCADE
+);
+
+CREATE TABLE courses_faculty (
+	course_id INT,
+	faculty_id INT,
+	FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE,
+	FOREIGN KEY (faculty_id) REFERENCES users(id) ON DELETE CASCADE	
+);
+
+CREATE TABLE course_students (
+	course_id INT,
+	student_id INT,
+	FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE,
+	FOREIGN KEY (student_id) REFERENCES users(id) ON DELETE CASCADE,
+);
