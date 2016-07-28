d6851f149c38bb48550a9d86becb545ca3e73d4c
10633 <lchen22@linuxlab08.mcs.slu.edu>
2016-07-28 15:47:55 -0500

    unknown update.

--- faculty/addACourse.php
+++ faculty/addACourse.php
@@ -4,8 +4,10 @@
 </head>
 <body>
 <?php include('addNewCourse.php'); ?>
+<form method="post" action="addNewCourse.php">
 <!--<input multiple type="CHECKBOX" name="newCourse[]" value=""><br>
 <input type="CHECKBOX" name="newCourse[]" value="CSCI2100"> CSCI2100 <br>-->
 <input type="submit" name="submit" value="Submit">
+</form>
 </body>
 </html>
--- faculty/addNewCourse.php
+++ faculty/addNewCourse.php
@@ -7,6 +7,7 @@
 //}
 include('conn.php');
 $query = mysql_query("select * from courses");
+$row = mysql_fetch_array($query);
 echo "<table border='1'>
 <tr>
 <th>Name and Section</th>
@@ -14,14 +15,17 @@ echo "<table border='1'>
 <th>Year</th>
 <th>Select</th>
 </tr>";
-while($row = mysql_fetch_array($query))
+while($row)
   {
   echo "<tr>";
   echo "<td>" . $row['name'] . "." . $row['section'] . "</td>";
   echo "<td>" . $row['semester'] . "</td>";
   echo "<td>" . $row['course_year'] . "</td>";
-  echo "<td><input type=\"CHECKBOX\" name=\"newCourse[]\" value=\"$row[id]\"></td>";
+  echo "<td><input type=\"CHECKBOX\" name=\"newCourse[]\" value=\"$row['id']\"></td>";
   echo "</tr>";
   }
 echo "</table>"
+
+echo 
+//$row['id']=$_POST['newCourse[]'];
 ?>
