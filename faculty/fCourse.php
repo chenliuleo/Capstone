d57a07937a755f78d31738a44843f375b0e34455
10633 <lchen22@linuxlab08.mcs.slu.edu>
2016-07-28 18:37:07 -0500

    redesigned faculty left hand side page.

---
+++ faculty/fCourse.php
@@ -0,0 +1,26 @@
+ <html>
+ <head>
+ </head>
+ <body>
+ <?php
+ echo "echo1";
+ session_start();
+ echo "echo";
+ include ('conn.php');
+ $username = $_SESSION['username'];
+ echo $username;
+ $user_query = mysql_query("select id from users where username='$username' limit 1");
+ $row = mysql_fetch_array($user_query);
+ $id = $row['id'];
+ echo $id;
+ /*$query = mysql_query("select course_id from courses where faculty_id='$id'");
+ $course_id = mysql_fetch_array($query);
+ foreach($course_id as $cid)
+ {
+  $course_detail = mysql_query("select name,section,semester,course_year from courses where id='$cid'");
+  $cdetail = mysql_fetch_array($course_detail);
+  echo "<a href=\\"facultyCourse.php\\">$cdetail['name']</a>";
+  }*/
+ ?>
+ </body>
+ </html>
