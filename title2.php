481563cc6899fac9a6775165a6ef4f0061108e19
ywang85 <ywang85@slu.edu>
2016-08-02 17:30:03 -0500

    add logout

--- title2.php
+++ title2.php
@@ -17,11 +17,17 @@
       width: 100;
       height: 120;
     }
+    .right{
+      text-align:right;
+    }
   </style>
   <body>
     <?php include('title.php');?>
     <center>
       <h1><font color="blue">Homework Submission System</font><br></h1>
+      <div class="right">
+	<a href="login.html" title="Log out" target="_parent">Log Out</a>
+      </div>
       <div id="content">
         <img src="SLU_Logo.jpg">
       </div>
