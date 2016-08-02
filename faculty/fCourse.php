1a11d38bca787af5cff9fe534e0c85092745888e
10633 <lchen22@linuxlab08.mcs.slu.edu>
2016-08-01 19:08:00 -0500

    fixed link;

--- faculty/fCourse.php
+++ faculty/fCourse.php
@@ -3,24 +3,19 @@
  </head>
  <body>
  <?php
- echo "echo1";
  session_start();
- echo "echo";
  include ('conn.php');
  $username = $_SESSION['username'];
- echo $username;
  $user_query = mysql_query("select id from users where username='$username' limit 1");
  $row = mysql_fetch_array($user_query);
  $id = $row['id'];
- echo $id;
  $query = mysql_query("select course_id from courses_faculty where faculty_id='$id'");
  $course_id = mysql_fetch_array($query);
  foreach($course_id as $cid)
  {
   $course_detail = mysql_query("select name,section,semester,course_year from courses where id='$cid'");
   $cdetail = mysql_fetch_array($course_detail);
-  echo $cdetail;
-  /*echo "<a href=\\"facultyCourse.php\\">$cdetail['name']</a>";*/
+  echo "<a href=\"faculty.html\">12345</a><br>";
   }
  ?>
  </body>
