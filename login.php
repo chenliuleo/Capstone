1361b856979919a867d9797998b6fd2d2b9bfe47
10633 <lchen22@linuxlab08.mcs.slu.edu>
2016-08-06 17:05:32 -0500

    delete my.php, auto-redirect to homepage.

--- login.php
+++ login.php
@@ -16,7 +16,8 @@ if(!isset($_POST['submit'])){
 }
 $username = htmlspecialchars($_POST['username']);
 $password = sha1($_POST['password']);
-
+//echo $username;
+//echo $password;
 //包含数据库连接文件
 include('conn.php');
 //检测用户名及密码是否正确
@@ -25,12 +26,30 @@ if($result = mysql_fetch_array($check_query)){
 	//登录成功
 	$_SESSION['username'] = $username;
 	$_SESSION['banner_id'] = $result['banner_id'];
-	echo 'Login success! <br />';
-	echo $username,' Welcome!This is <a href="my.php">Home page</a><br />';
-	echo 'Click here to <a href="login.php?action=logout">logoff</a><br />';
-	
+	//echo 'Login success! <br />';
+	//echo $username,' Welcome!This is <a href="my.php">Home page</a><br />';
+	//echo 'Click here to <a href="login.php?action=logout">logoff</a><br />';
+	//$username = $_SESSION['username'];
+	$user_query = mysql_query("select * from users where username='$username' limit 1");
+	$row = mysql_fetch_array($user_query);
+	//echo 'User information:<br />';
+	//echo 'Banner ID: ',$row['banner_id'],'<br />';
+	//echo 'Username: ',$username,'<br />';
+
+	//echo 'User type: ',$row['user_type'],'<br />';
+	//echo '<a href="login.php?action=logout">Logout</a><br />';
+
+	if($row['user_type'] == 'Student')
+	{
+	  echo "<meta http-equiv=\"refresh\" content=\"0; ./student/mainpage.html\" />";
+	}
+	else
+	{
+	  echo "<meta http-equiv=\"refresh\" content=\"0; ./faculty/fMainpage.html\" />";
+	}
 	exit;
-} else {
-	exit('Login failed! Click here <a href="javascript:history.back(-1);">back</a> Try again');
+	} 
+else {
+  exit('Login failed! Click here <a href="javascript:history.back(-1);">back</a> Try again');
 }
 ?>
