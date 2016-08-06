3c3414fa2dbff0a1b55d4db431c86b878875b722
ywang85 <ywang85@slu.edu>
2016-08-06 18:07:27 -0500

    change font

--- faculty/studentHomework.php
+++ faculty/studentHomework.php
@@ -1,6 +1,8 @@
 <html>
 <head>
-
+<style>
+  p{font-family: Arial, Helvetica, sans-serif;}
+</style>
 </head>
 <body>
 <?php
@@ -19,10 +21,10 @@ $student_homework = mysql_fetch_array($student_homework_query);
 $student_info_query = mysql_query("select * from users where id='$student_id'");
 $student_info = mysql_fetch_array($student_info_query);
 
-echo "Homework: " . $homework['title'] . "<br>";
+echo "<p>Homework: " . $homework['title'] . "<br>";
 echo "Description: " . $homework['description'] . "<br>";
 echo "Student Name: " . $student_info['first_name'] . " " . $student_info['last_name'] . "<br>";
-echo "Full Score: " . $homework['total_points'] . "<br>";
+echo "Full Score: " . $homework['total_points'] . "<br></p>";
 echo "<hr>";
 $get_file_sql = mysql_query("select id from files where user_id='$student_id' and hw_id='$homework_id' order by submit_time,filename");
 $get_file_array = Array();
@@ -33,16 +35,16 @@ foreach($get_file_array as $fid){
   $get_filename = mysql_query("select filename from files where id='$fid'");
   $filename_array = mysql_fetch_array($get_filename);
   $filename = $filename_array['filename'];
-  echo "File name: ";
+  echo "<p>File name: ";
   echo $filename;
   echo " ";
   echo "<a href=\"http://turing.slu.edu/~lchen22/cgi-bin/download.py?name=/export/mathcs/home/student/l/lchen22/WWW/upload/$filename\">Download</a>";
-  echo "<br>";
+  echo "<br><p/>";
 }
 echo "<hr>";
 echo "<form action=\"enterScore.php?id=$course_id&banner=$banner_id&hwid=$homework_id&stdid=$student_id\">";
-echo "Enter Score: " . "<input id='earned_points' type='int' name='earned_points' /><br>";
-echo "Enter Feedback: " . "<input id='feedback' type='text' name='feedback' /><br>";
+echo "<p>Enter Score: " . "<input id='earned_points' type='int' name='earned_points' /><br><br>";
+echo "Enter Feedback: " . "<br><textarea rows='4' cols='50' id='feedback' type='text' name='feedback'></textarea><br></p>";
 echo "<input type='hidden' name='id' value='$course_id' />";
 echo "<input type='hidden' name='banner' value='$banner_id' />";
 echo "<input type='hidden' name='hwid' value='$homework_id' />";
