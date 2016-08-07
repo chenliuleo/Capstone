ee46a821504360970224e27e671c8731e94bf84b
10633 <lchen22@linuxlab08.mcs.slu.edu>
2016-08-07 00:16:33 -0500

    fixed faculty home page

--- faculty/facultyHome.php
+++ faculty/facultyHome.php
@@ -5,14 +5,17 @@
     <title>Homework Submission System</title>
   </head>
   <body>
-  <h3><a href="./addACourse.php">add a course</a></h3>
-  <h3>Course this semester: <br></h3>
+  <!--<h3>Course this semester: <br></h3>
   <h3>Homework: <br></h3>
-  <h3>Coming due: <br></h3>
+  <h3>Coming due: <br></h3>-->
   <?php
   session_start();
   include('conn.php');
-  
+  echo "Welcome!";
+  echo "<br>";
+  //echo "Coming due: ";
+  echo "<br>";
+
   ?>
   </body>
 </html>
