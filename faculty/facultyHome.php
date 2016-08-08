9d8c59dd1ae8c84e2b0e663d633e6ece329b9908
ywang85 <ywang85@slu.edu>
2016-08-08 04:18:08 -0500

    update

--- faculty/facultyHome.php
+++ faculty/facultyHome.php
@@ -3,6 +3,17 @@
 <html>
   <head>
     <title>Homework Submission System</title>
+    <style>
+      table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+      text-align: center;
+      }
+      th, td {
+      padding: 5px;
+      }
+      table {font-family: Arial, Helvetica, sans-serif;}
+    </style>
   </head>
   <body>
   <h3><a href="./addACourse.php">add a course</a></h3>
