a6353834b3e64f2207149249ccaa5112e666f8ce
Liu Chen <chenliuleo@gmail.com>
2016-08-08 05:55:24 -0500

    Update deleteScript.php

--- faculty/deleteScript.php
+++ faculty/deleteScript.php
@@ -4,16 +4,16 @@ $name=$_GET['deleteStudent'];
 include ('conn.php');
 $username = $_SESSION['username'];
 $course_id = $_SESSION['course_id'];
-var_dump($name);
+//var_dump($name);
 echo "<br>";
-var_dump($course_id);
+//var_dump($course_id);
 //echo $username;
 //echo $id;
 foreach($name as $temp){
 //  echo $temp;
   $courses_faculty = mysql_query("delete from course_students where student_id='$temp' and course_id='$course_id'");
   }
-//echo "<meta http-equiv='refresh' content='0; deleteStudent.php?id=$course_id' />";
+echo "<meta http-equiv='refresh' content='0; deleteStudent.php?id=$course_id' />";
 //$message="Add course succes!";
 //echo "<script type='text/javscript'>alert('$message');</script>";  
 ?>
